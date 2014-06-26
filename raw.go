5e7ca6d4e552b275ddfc28fe78a7b74dc167844f
Ben Johnson <benbjohnson@yahoo.com>
2014-06-25 21:29:52 -0600

    Change raw.String to use a uint16 offset and len.

--- raw.go
+++ raw.go
@@ -9,20 +9,20 @@ import (
 
 // String represents an offset and pointer to a string in a byte slice.
 type String struct {
-	Offset int
-	Length int
+	Offset uint16
+	Length uint16
 }
 
 // Encode writes a string to a byte slice and updates the offset/length.
 func (s *String) Encode(str string, value *[]byte) {
-	s.Offset = len(*value)
-	s.Length = len(str)
+	s.Offset = uint16(len(*value))
+	s.Length = uint16(len(str))
 	*value = append(*value, []byte(str)...)
 }
 
 // Bytes returns a byte slice pointing to the string's contents.
 func (s *String) Bytes(value []byte) []byte {
-	return (*[0xFFFFFFF]byte)(unsafe.Pointer(&value[s.Offset]))[:s.Length]
+	return (*[0xFFFF]byte)(unsafe.Pointer(&value[s.Offset]))[:s.Length]
 }
 
 // String returns a Go string of the string value from an encoded byte slice.
